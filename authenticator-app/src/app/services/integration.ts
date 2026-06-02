70cfa641967e1b003c6742a1f9238a932b774670
Akshay1820 <akshuvaidya1@gmail.com>
2026-06-02 22:40:41 +0530

    Added environment variables

--- authenticator-app/src/app/services/integration.ts
+++ authenticator-app/src/app/services/integration.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Integration {
 
-  API_URL = 'http://localhost:8080';
+  API_URL = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
